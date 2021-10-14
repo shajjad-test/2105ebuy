2a7f754dea0182992d5e4cbedb20829f048a6245
tariquedev <tariquehasan19@gmail.com>
2021-10-14 20:08:15 +0600

    catagory done

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    'use strict';
+
+
+    //banner slider;
+    $('.banner-slider').slick({
+        prevArrow: '<i class="fas fa-chevron-left left-arrow"></i>',
+        nextArrow: '<i class="fas fa-chevron-right right-arrow"></i>',
+    });
+});
